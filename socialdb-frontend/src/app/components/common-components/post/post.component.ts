98bfafcbe64d7e546f06464376aeec1915a70078
diegovcandal <diego.varela970@gmail.com>
2022-10-02 13:50:07 +0200

    SOCIALDB-1: Added more post content cases to control

--- socialdb-frontend/src/app/components/common-components/post/post.component.ts
+++ socialdb-frontend/src/app/components/common-components/post/post.component.ts
@@ -16,10 +16,11 @@ export class PostComponent implements OnInit {
     public safeUrl: SafeUrl;
     public videoUrl: string;
 
+    @ViewChild('imageContainer') imageContainer: ElementRef;
     @ViewChild('iframeContainer') iframeContainer: ElementRef;
 
-    public height: string = '100%';
-    public width: string = '100%';
+    public iframeHeight: string = '100%';
+    public iframeWidth: string = '100%';
     public iframeStyle: string = 'position:relative; padding-bottom:calc(100% + 200px)';
     public aspectRatio: number = 0;
     public allow: string = '';
@@ -28,13 +29,81 @@ export class PostComponent implements OnInit {
 
     public videoControls: boolean;
 
+    public testclass: string = 'post-media-container';
+    public open: boolean = false;
+    public baseHeigh: number;
+
+    public carouselTestArray: Array<any> = new Array<any>;
+    public isHiddenContent: boolean = true;
+    public loaded: boolean = false;
+
     constructor(
         private domSanitazer: DomSanitizer
     ) { }
 
     ngOnInit(): void {
         this.getType();
-        //console.log(this.postData)
+    }
+
+    testCarga(id?: number) {
+
+        if (!this.loaded) {
+            this.loaded = true;
+        }
+
+        if (this.mediaType === 'image') {
+            console.log("hei:  " + this.imageContainer.nativeElement.scrollHeight)
+            if (this.imageContainer.nativeElement.scrollHeight > 300) {
+                this.baseHeigh = this.imageContainer.nativeElement.scrollHeight;
+            }
+        } else if (this.mediaType === 'carousel') {
+            console.log('test gallery: ' + this.imageContainer.nativeElement.offsetHeight)
+            if (this.imageContainer.nativeElement.scrollHeight > 300) {
+                this.carouselTestArray.push({ id: id, baseHeigh: this.imageContainer.nativeElement.scrollHeight })
+            }
+        }
+    }
+
+    test(id?: number) {
+        if (this.mediaType === 'image') {
+
+            if (this.baseHeigh >= 300) {
+                console.log(this.open)
+                if (!this.open) {
+                    console.log("si")
+                    this.open = true;
+                    this.testclass = '';
+                } else {
+                    console.log("no")
+                    this.open = false;
+                    this.testclass = 'post-media-container';
+                }
+
+            }
+        } else if (this.mediaType === 'carousel') {
+            let entry = this.carouselTestArray.filter(e => id === e.id)[0]
+            if (entry.baseHeigh >= 300) {
+                console.log(this.open)
+                if (!this.open) {
+                    console.log("si")
+                    this.open = true;
+                    this.testclass = '';
+                } else {
+                    console.log("no")
+                    this.open = false;
+                    this.testclass = 'post-media-container';
+                }
+
+            }
+        }
+    }
+
+    showContent() {
+        if (!this.isHiddenContent) {
+            this.isHiddenContent = true;
+        } else {
+            this.isHiddenContent = false;
+        }
     }
 
     ngAfterViewInit() {
@@ -43,10 +112,16 @@ export class PostComponent implements OnInit {
         }, 0);
     }
 
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        if (this.mediaType === 'iframe' && this.iframeWidth !== '100%') {
+            this.resizeIframe();
+        }
+    }
+
     private getType() {
 
         if (this.postData.post_hint && this.postData.post_hint === 'hosted:video') {
-
             this.mediaType = 'video';
             this.videoUrl = this.postData.secure_media.reddit_video.fallback_url
             this.videoControls = true;
@@ -78,18 +153,11 @@ export class PostComponent implements OnInit {
 
         } else if (this.postData.post_hint && this.postData.post_hint === 'image') {
             this.mediaType = 'image';
-            
+
         } else {
             this.mediaType = 'no_content';
         }
 
-    }
-
-    @HostListener('window:resize', ['$event'])
-    onResize() {
-        if (this.width !== '100%') {
-            this.resizeIframe();
-        }
     }
 
     private loadIframeData(oembed: RedditOembed, url: string, type: string): string {
@@ -99,20 +167,19 @@ export class PostComponent implements OnInit {
         this.iframeStyle = '';
 
         if (type === 'youtube.com') {
-            url = 'https://www.youtube.com/embed/' + this.postData.url.split('watch?v=')[1]
+            newUrl = 'https://www.youtube.com/embed/' + newUrl.split('watch?v=')[1]
             this.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
         } else {
-            this.width = oembed.width.toString();
-            this.height = oembed.height.toString();
+            this.iframeWidth = oembed.width.toString();
+            this.iframeHeight = oembed.height.toString();
         }
 
         return newUrl;
     }
 
-
     private resizeIframe() {
         if (this.iframeContainer) {
-            this.height = (this.aspectRatio * this.iframeContainer.nativeElement.offsetWidth).toString();
+            this.iframeHeight = (this.aspectRatio * this.iframeContainer.nativeElement.offsetWidth).toString();
         }
     }
 
